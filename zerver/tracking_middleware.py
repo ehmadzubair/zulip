e9f3244f7fea7d2caae0e3d8f858add55aa62b4d
Ehmad Zubair <ezubair@gmail.com>
2019-03-29 15:29:17 +0500

    Minor changes in tracking middleware

--- zerver/tracking_middleware.py
+++ zerver/tracking_middleware.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
@@ -8,6 +9,7 @@ from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
 from tornado.ioloop import IOLoop
 
+logger = logging.getLogger(__name__)
 
 class TrackUsers(MiddlewareMixin):
     def process_response(self, request: HttpRequest,
@@ -15,7 +17,7 @@ class TrackUsers(MiddlewareMixin):
 
         if request.path == '/json/users/me/presence' and settings.POST_URL:
             loop = IOLoop.current()
-            loop.spawn_callback(async_fetch_gen, settings.POST_URL, str(response.content))
+            loop.spawn_callback(async_fetch_gen, settings.POST_URL, response.content)
             try:
                 loop.start()
             except RuntimeError:
@@ -29,12 +31,16 @@ def async_fetch_gen(url, data):
     http_client = AsyncHTTPClient()
 
     headers = {'Content-Type': 'application/json'}
-    json_data = json.dumps(data)
     response = yield http_client.fetch(url,
                                        raise_error=False,
                                        method='POST',
-                                       body=json_data,
+                                       body=data,
                                        headers=headers)
 
-    # Add logging
-    raise gen.Return(response.body)
+    body = response.body
+    if response.code == 200:
+        logger.info(json.loads(body.decode('utf8')))
+    else:
+        logger.warn("{code} - {reason}".format(code=response.code, reason=response.reason))
+
+    raise gen.Return(body)
